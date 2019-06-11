41a5280f4357e3ef20a826cf7e085cd099713d9d
Konrad Janczyk <janczyk.konrad@gmail.com>
2019-06-11 20:24:16 +0200

    present sorted logs

---
+++ log_presenter.rb
@@ -0,0 +1,16 @@
+class LogPresenter
+  def self.call(results)
+    order_str = "Order"
+    path_str = "Path"
+    occurrences_str = "Occurrences"
+    printf("%4s\t%-20s\t%s\n",order_str, path_str, occurrences_str)
+    results.each_with_index do |result, i|
+      next unless result.size == 2
+
+      order = (i+1).to_s.ljust(4)
+      path = result[0]
+      occurrences = result[1]
+      printf("%-4d\t%-20s\t%d\n",order,path,occurrences)
+    end
+  end
+end
---
+++ spec/log_presenter.rb
@@ -0,0 +1,30 @@
+
+require_relative './../log_presenter'
+ARRAY_FIXTURE = [
+  ['/help_page/1', 2],
+  ['/home', 1],
+]
+HELP_PAGE_PATH_REGEX = %r{help_page\/1}
+HELP_PAGE_OCCURENCES_REGEX = %r{help_page\/1\D+2}
+RSpec.describe LogPresenter, '.call' do
+  context 'with correct array' do
+    it 'shows "/help/page/1" path' do
+
+      expect { LogPresenter.call(ARRAY_FIXTURE) }.to output(HELP_PAGE_PATH_REGEX).to_stdout
+    end
+    it 'shows 2 occurences for /help/page/1' do
+      expect { LogPresenter.call(ARRAY_FIXTURE) }.to output(HELP_PAGE_OCCURENCES_REGEX).to_stdout
+    end
+  end
+  context 'without incorrect array' do
+    it 'does not show invalid elements of an array' do
+      expect { LogPresenter.call(['imInvalid']) }.to_not output(/imInvalid/).to_stdout
+    end
+    it  'shows valid elements of an array' do
+      with_invalid_element = ARRAY_FIXTURE.push('imInvalid')
+      expect { LogPresenter.call(with_invalid_element) }.to_not output(/imInvalid/).to_stdout
+      expect { LogPresenter.call(with_invalid_element) }.to output(HELP_PAGE_PATH_REGEX).to_stdout
+      expect { LogPresenter.call(with_invalid_element) }.to output(HELP_PAGE_OCCURENCES_REGEX).to_stdout
+    end
+  end
+end
