835f5f03be7f9e8368267bca448560cf2663ec73
Konrad Janczyk <janczyk.konrad@gmail.com>
2019-06-11 21:38:58 +0200

    fix spaces

--- spec/log_presenter.rb
+++ spec/log_presenter.rb
@@ -9,7 +9,6 @@ HELP_PAGE_OCCURENCES_REGEX = %r{help_page\/1\D+2}
 RSpec.describe LogPresenter, '.call' do
   context 'with correct array' do
     it 'shows "/help/page/1" path' do
-
       expect { LogPresenter.call(ARRAY_FIXTURE) }.to output(HELP_PAGE_PATH_REGEX).to_stdout
     end
     it 'shows 2 occurences for /help/page/1' do
@@ -20,7 +19,7 @@ RSpec.describe LogPresenter, '.call' do
     it 'does not show invalid elements of an array' do
       expect { LogPresenter.call(['imInvalid']) }.to_not output(/imInvalid/).to_stdout
     end
-    it  'shows valid elements of an array' do
+    it 'shows valid elements of an array' do
       with_invalid_element = ARRAY_FIXTURE.push('imInvalid')
       expect { LogPresenter.call(with_invalid_element) }.to_not output(/imInvalid/).to_stdout
       expect { LogPresenter.call(with_invalid_element) }.to output(HELP_PAGE_PATH_REGEX).to_stdout
